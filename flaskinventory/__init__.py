5cf16677371454d3831e84acd2d4e65e73f278e5
Folk N <81551496+folkadonis@users.noreply.github.com>
2024-06-12 15:39:57 +0530

    zxc

--- flaskinventory/__init__.py
+++ flaskinventory/__init__.py
@@ -1,15 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '323b22caac41acbf'
 app.config['SQLALCHEMY'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
 from flaskinventory import routes
-
 db.create_all()
 db.session.commit()
